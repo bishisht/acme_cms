5b76f23a133b6dea02b5483d4ac057ef329b0e05
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2015-06-17 23:45:17 +0545

    Locomotive Working Fine. Congrats :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcmeCms::Application.config.secret_token = '133e85e3b666f6abc961e48b07f5ec9ca63e28283e79f9f040071b9f82b95dcf98209bcb9fdc9aa16c63f40b07f061655ac4728a2caf32aab58b43d2596a8563'
